a801704794862cd1d38f962fca14e4cb53bac5ba
lijunyi01 <lijunyi01@gmail.com>
2018-05-27 13:41:22 +0800

    modify

--- modules/springcloudlearn-parent/service-consumer-movie/src/main/java/com/allcom/Application.java
+++ modules/springcloudlearn-parent/service-consumer-movie/src/main/java/com/allcom/Application.java
@@ -2,10 +2,12 @@ package com.allcom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
     @Bean
     public RestTemplate restTemplate(){
--- modules/springcloudlearn-parent/service-provider-user/src/main/java/com/allcom/Application.java
+++ modules/springcloudlearn-parent/service-provider-user/src/main/java/com/allcom/Application.java
@@ -2,8 +2,14 @@ package com.allcom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//等价于@EnableDiscoveryClient，只是语义更明确，但只能用于注册到eureka
+//而@EnableDiscoveryClient 可以注册到zk等其它服务注册器
+@EnableEurekaClient
+//@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
