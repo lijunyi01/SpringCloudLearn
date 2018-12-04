60c4e1850a301466246e4db6cb89cbcf1ea41bf2
lijunyi01 <lijunyi01@gmail.com>
2018-12-04 19:19:12 +0800

    modify

--- modules/springcloudlearn-parent/spring-config-client/src/main/java/com/allcom/ConfigClientApplication.java
+++ modules/springcloudlearn-parent/spring-config-client/src/main/java/com/allcom/ConfigClientApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConfigClientApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientApplication.class, args);
+        SpringApplication.run(ConfigClientEurekaApplication.class, args);
     }
 }
 
